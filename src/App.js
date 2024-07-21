e63b629fffbfcc21f27f09398f5fc622af718767
unknown <sarveshbattenapati@gmail.com>
2024-07-21 19:56:48 +0530

    Uploaded on 21st July, 2024

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
 import React, { useState } from 'react';
-import { account, ID } from './lib/appwrite';
-import {useUser} from 'react';
 
-
+import { databases } from './lib/appwrite';
+import { ID } from 'appwrite';
 
 function App() {
-const user = useUser();
-const [email,setEmail] = useState("");
 
+  const [success,setSuccess] = useState(false);
+  const [name,setName] = useState("");
+  const [email,setEmail] = useState("");
+  
+  const submitUser = async () => {
+    try {
+      
+      await databases.createDocument("669a6299000fb18aa727", "669d0ee2002f3b8f0f6d",//database, collection
+      ID.unique(),
+       {
+        Name:name,
+        Email: email
+        
+      });
+      setSuccess(true);
+    } catch (error) {
+      console.log(error);
+    }
+  }
   return (
 
     <div className="EmailApp">
-      Email:<input type="text" id="EmailInput" placeholder='Email' value={email}></input>
-      <button type="button" onClick={()=>user.login(email)}>Login</button>
-      <button type="button" onClick={()=>user.register(email)}>Register</button>
-      
+
+
+      Name:<input type = "text" placeholder='Enter your name here' id='NameInput' value = {name} 
+      onChange={val => setName(val.target.value)} ></input>
+     
+
+      Email:<input type="email" id="EmailInput" placeholder='Email' value={email} 
+      onChange={val => setEmail(val.target.value)}></input>
+
+
+
+      <button type="button" onClick={submitUser}>Register</button>
+      <div>
+        {success && <div>Successfully submitted user.</div>}
+      </div>
     </div>
   );
 }
-
 export default App;
--- src/lib/appwrite.js
+++ src/lib/appwrite.js
@@ -1,12 +1,23 @@
 
-//import './App.css';
-import { Client, Account} from 'appwrite';
 
-export const client = new Client();
+import { Client, Databases} from 'appwrite';
 
-client
+const client = new Client()
     .setEndpoint('https://cloud.appwrite.io/v1')
     .setProject('669a5fe800279787b59e'); // Replace with your project ID
-    
-export const account = new Account(client);
-export { ID } from 'appwrite';
+
+
+export const databases = new Databases(client);
+
+
+let promise = databases.listDocuments(
+    "669a6299000fb18aa727",
+    "669d0ee2002f3b8f0f6d",
+
+);
+
+promise.then(function (response) {
+    console.log(response);
+}, function (error) {
+    console.log(error);
+});
